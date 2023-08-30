2033e3350bee3beb26e24b68b46c78bfee241ef5
MazanSM <mazansm@mail.uc.edu>
2023-08-29 23:48:14 -0400

    End of Section 3

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -16,6 +16,8 @@
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
         public DateTime AuctionEnd { get; set; }
 
         public Status Status { get; set; }
